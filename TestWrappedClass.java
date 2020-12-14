ac817b348cc76f0d3bcf36efd8622ba02b7ffb5d
zykgithub1 <787950451@qq.com>
2020-12-14 22:08:22 +0800

    测试

---
+++ TestWrappedClass.java
@@ -0,0 +1,14 @@
+package cn.sxt.test;
+
+public class TestWrappedClass {
+	public static void main(String[]args) {
+		Integer i=new Integer(3); 
+		Integer b=Integer.valueOf(30);
+		int c=b.intValue();
+		Integer a=2345;
+		int d=a;
+		System.out.println(b);
+		
+	}
+
+}
